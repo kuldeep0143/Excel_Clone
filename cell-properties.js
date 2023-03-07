5c9606d2a56067d2a361eb119c42a80b12f0c034
Kuldeep Chouhan <90789809+kuldeep0143@users.noreply.github.com>
2023-03-07 20:15:25 +0530

    Update cell-properties.js

--- cell-properties.js
+++ cell-properties.js
@@ -7,27 +7,6 @@ let sheetDB = [];
     addSheetBtn.click();
 }
 
-// for (let i = 0; i < rows; i++) {
-//     let sheetRow = [];
-//     for (let j = 0; j < cols; j++) {
-//         let cellProp = {
-//             bold: false,
-//             italic: false,
-//             underline: false,
-//             alignment: "left",
-//             fontFamily: "monospace",
-//             fontSize: "14",
-//             fontColor: "#000000",
-//             BGcolor: "#000000",  // Just for indication purpose,
-//             value: "",
-//             formula: "",
-//             children: [],
-//         }
-//         sheetRow.push(cellProp);
-//     }
-//     sheetDB.push(sheetRow);
-// }
-
 
 // Selectors for cell properties
 let bold = document.querySelector(".bold");
